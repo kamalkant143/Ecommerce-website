9a562e6a7eef28b40a27eb19d8f874db20f4d6c3
kamal kant <kamalshakya14311@gmail.com>
2024-04-27 19:14:23 +0530

    media quary

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Script for navigation bar
+
+let bar=document.querySelector("#bar");
+let navBar =document.querySelector("#heading .nav-bar .nav-bar-list")
+
+console.log(navBar);
+
+bar.addEventListener("click",(navBar)=>{
+//  console.log(navBar.style.right="30rem");
+});
