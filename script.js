7a0ba1386f742f976bd8b43b5562048db9de79df
kamal kant <kamalshakya14311@gmail.com>
2024-05-04 18:30:46 +0530

    about html

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 let bar=document.querySelector("#bar");
 let navBar =document.querySelector(".nav-bar-list")
 let close=document.querySelector("#close")
-
-
+let image=document.querySelector(".main-box")
 
 if(bar){
   bar.addEventListener("click",()=>{
@@ -17,9 +16,7 @@ if(close){
     })
   };
 
-  let image=document.querySelector(".main-box")
 
   image.addEventListener("click",()=>{
    window.location.href="http://127.0.0.1:5500/sproduct.html"
   })
-
